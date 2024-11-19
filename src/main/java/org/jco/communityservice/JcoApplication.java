fac42975675d414c0c93d937ec28af156bc815a9
taedong <rlaxoehd4234@naver.com>
2024-11-20 00:59:45 +0900

    Add a root project

---
+++ src/main/java/org/jco/communityservice/JcoApplication.java
@@ -0,0 +1,13 @@
+package org.jco.communityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jco/communityservice/JcoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jco.communityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
