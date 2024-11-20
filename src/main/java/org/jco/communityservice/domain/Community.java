06cbb380b490bb0d0fb55d40a6d0d0131589501f
taedong <rlaxoehd4234@naver.com>
2024-11-20 10:59:45 +0900

    record entity -> class entity CHANGE

--- src/main/java/org/jco/communityservice/domain/Community.java
+++ src/main/java/org/jco/communityservice/domain/Community.java
@@ -1,19 +1,27 @@
 package org.jco.communityservice.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.Date;
 
-public record Community(
-        Long ComId,
-        String title,
-        String content,
-        //category 추가 예정 -> jpa 의존성 추가 후
-        Long likeCount,
-        Long unlikeCount,
-        Date enrollTime,
-        Date updateTime,
-//        int deleteYn
-        Long view,
-        String writer,
-        String image_root
-)
-{}
+@Entity
+public class Community
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long ComId;
+    String title;
+    String content;
+    //category 추가 예정 -> jpa 의존성 추가 후
+    Long likeCount;
+    Long unlikeCount;
+    Date enrollTime;
+    Date updateTime;
+    //        int deleteYn
+    Long view;
+    String writer;
+    String image_root;
+}
