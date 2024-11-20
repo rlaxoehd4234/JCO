4825a05566844044b6581d6c66d777dea2a21e65
taedong <rlaxoehd4234@naver.com>
2024-11-20 21:32:27 +0900

    생성자 주입 수정

--- src/main/java/org/jco/communityservice/domain/CommunityService.java
+++ src/main/java/org/jco/communityservice/domain/CommunityService.java
@@ -1,8 +1,10 @@
 package org.jco.communityservice.domain;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CommunityService {
     private final CommunityRepository communityRepository;
 
