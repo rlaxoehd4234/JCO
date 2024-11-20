f2676ed083c7f8a51ec1f68faee431b332151aee
taedong <rlaxoehd4234@naver.com>
2024-11-20 18:51:02 +0900

    [REFACTOR] JPA 에서 UPDATE -> 쿼리 삭제 후 추후 추가 예정

--- src/main/java/org/jco/communityservice/domain/CommunityRepository.java
+++ src/main/java/org/jco/communityservice/domain/CommunityRepository.java
@@ -1,14 +1,15 @@
 package org.jco.communityservice.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CommunityRepository extends JpaRepository<Community, Long> {
     List<Community> findAll();
-    Optional<Community> findByComId(Long id);
-    Long save(Community community);
+//    Optional<Community> findByComId(Long id);
+    Community save(Community community);
     void delete(Community community);
-    void updateByComId(Long id, Community community);
 }
