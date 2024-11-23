ebcb24ee66c779f261659d916130ec75d458ab10
taedong <rlaxoehd4234@naver.com>
2024-11-23 20:38:30 +0900

    [TEST] CommunityController findById Method -> NotFoundException 확인 테스트

--- src/test/java/org/jco/communityservice/domain/CommunityControllerTest.java
+++ src/test/java/org/jco/communityservice/domain/CommunityControllerTest.java
@@ -2,19 +2,32 @@ package org.jco.communityservice.domain;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
+
+//@SpringBootTest(
+//        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+//)
+@WebMvcTest(controllers = CommunityController.class)
 class CommunityControllerTest {
-
+    @Autowired
+    private MockMvc mockMvc;
     @Autowired
     private WebTestClient webTestClient;
+
+    @MockBean
+    private CommunityService service;
+    @Autowired
+    private CommunityService communityService;
 
     @Test
     void getCommunity() {
@@ -32,6 +45,16 @@ class CommunityControllerTest {
                 .expectStatus().isCreated()
                 .expectBody(Integer.class)
                 .value(actual -> assertThat(actual).isEqualTo(1));
+    }
+
+    @Test
+    void whenGetCommunityNotFound() throws Exception{
+        long communityNumber = 10;
+        given(communityService.findById(communityNumber))
+                .willThrow(CommunityNotFoundException.class);
+        mockMvc
+                .perform(get("/community/find/"+communityNumber))
+                .andExpect(status().isNotFound());
     }
 
 
