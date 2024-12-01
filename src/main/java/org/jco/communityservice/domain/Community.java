a2fa13a988b763ce98a2c11541e4925acd11d49a
taedong <rlaxoehd4234@naver.com>
2024-12-01 18:33:38 +0900

    [TEST] Test 더미 데이터 100만건 추가

--- src/main/java/org/jco/communityservice/domain/Community.java
+++ src/main/java/org/jco/communityservice/domain/Community.java
@@ -14,9 +14,11 @@ import java.util.Date;
 @Getter
 public class Community {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     int comId;
+    @Column(length = 100)
     String title;
+    @Column(length = 1000)
     String content;
     //category 추가 예정 -> jpa 의존성 추가 후
     Long likeCount;
@@ -39,6 +41,16 @@ public class Community {
         this.title = title;
         this.content = content;
         this.imageRoot = imageRoot;
+    }
+
+    public Community(String title, String content, Long likeCount, Long unlikeCount, String writer, String imageRoot) {
+        this.title = title;
+        this.content = content;
+        this.likeCount = likeCount;
+        this.unlikeCount = unlikeCount;
+        this.writer = writer;
+        this.imageRoot = imageRoot;
+        this.view = 0L; // 기본 값 설정
     }
 
     // PrePersist 를 활용해 likeCount, UnlikeCount 의 default 값을 설정해줍니다.
